1e59ca8fffdc1e6368fb94a827578ae5051317bd
Sergio <sergioadm120@gmail.com>
2023-04-20 17:41:32 -0300

    adding new methods to process requests with Thymeleaf

--- regescweb/src/main/java/br/com/ifspcodelab/regescweb/controllers/HelloController.java
+++ regescweb/src/main/java/br/com/ifspcodelab/regescweb/controllers/HelloController.java
@@ -1,12 +1,29 @@
 package br.com.ifspcodelab.regescweb.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request) {
+        request.setAttribute("nome", "Sergio");
         return "hello"; //O Spring vai retornar o arquivo templates/hello.html
+    }
+
+    @GetMapping("/hello-model")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Sergio Gabriel");
+        return "hello"; //O Spring vai retornar o arquivo templates/hello.html
+    }
+
+    @GetMapping("/hello-mview")
+    public ModelAndView hello() {
+        ModelAndView mv = new ModelAndView("hello"); //Nome do arquivo html a ser exibido
+        mv.addObject("nome", "Sergio Gabriel de Lima Aquino");
+        return mv; //O Spring vai retornar (renderizar) o arquivo templates/hello.html
     }
 }

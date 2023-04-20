dced9ea6ef08317464be54525ce7cf1978d7e698
Sergio <sergioadm120@gmail.com>
2023-04-20 13:56:13 -0300

    Data Base implementation

--- regescweb/src/main/java/br/com/ifspcodelab/regescweb/RegescwebApplication.java
+++ regescweb/src/main/java/br/com/ifspcodelab/regescweb/RegescwebApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.ifspcodelab.regescweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class RegescwebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegescwebApplication.class, args);
 	}
-
 }

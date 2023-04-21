6d1845a17341ef017ca952e30f349cafd52cc38b
Sergio <sergioadm120@gmail.com>
2023-04-21 11:19:39 -0300

    pushing data from the database

--- regescweb/src/main/java/br/com/ifspcodelab/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/br/com/ifspcodelab/regescweb/controllers/ProfessorController.java
@@ -2,6 +2,8 @@ package br.com.ifspcodelab.regescweb.controllers;
 
 import br.com.ifspcodelab.regescweb.models.Professor;
 import br.com.ifspcodelab.regescweb.models.StatusProfessor;
+import br.com.ifspcodelab.regescweb.repositories.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,21 +14,30 @@ import java.util.List;
 
 @Controller
 public class ProfessorController {
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    /*public ProfessorController(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }*/ //Mesma coisa que o @Autowired do ProfessorRpository
+
     @GetMapping("/professores")
     public ModelAndView index() {
-        Professor batman = new Professor("Batman", new BigDecimal(5000), StatusProfessor.ATIVO);
+       /* Professor batman = new Professor("Batman", new BigDecimal(5000), StatusProfessor.ATIVO);
         batman.setId(1l);
 
         Professor coringa = new Professor("Coringa", new BigDecimal(10000), StatusProfessor.APOSENTADO);
         coringa.setId(2l);
 
         Professor mulherMaravilha = new Professor("Mulher Maravilha", new BigDecimal(15000), StatusProfessor.INATIVO);
-        mulherMaravilha.setId(3l);
+        mulherMaravilha.setId(3l); */
 
-        List<Professor> professores = Arrays.asList(batman, coringa, mulherMaravilha);
+        List<Professor> professores = this.professorRepository.findAll();
 
         ModelAndView mv = new ModelAndView("professores/index");
         mv.addObject("professores", professores);
         return mv;
     }
 }
+
+
---
+++ regescweb/src/main/java/br/com/ifspcodelab/regescweb/repositories/ProfessorRepository.java
@@ -0,0 +1,8 @@
+package br.com.ifspcodelab.regescweb.repositories;
+
+import br.com.ifspcodelab.regescweb.models.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> { }
